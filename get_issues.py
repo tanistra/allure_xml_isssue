77a6cd4f1e08ba346546da07f4991b0b9cdf4d1a
tanistra <Jaroslaw.Tanistra@smart4aviation.aero>
2017-06-07 12:28:17 +0200

    update script

--- get_issues.py
+++ get_issues.py
@@ -5,8 +5,8 @@ import os
 import logging
 from time import sleep
 
-ISSUES = []
-DUPLICATES = []
+ISSUES = [{'issues': 'JIRA NUMBER', 'name': 'TEST NAME', 'description': 'TEST DESCRIPTION',
+           'test status': 'EXECUTION STATUS', 'time': 'EXECUTION TIME', 'error': "ERROR MESSAGE"}]
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 logger = logging.getLogger(__name__)
@@ -22,21 +22,29 @@ def read_xml_test_data(file, path):
 
     test_suite = e.find("test-cases")
     test_case = test_suite.findall("test-case")
-
+    error = ''
     for test in test_case:
         name = test.find('name').text
+        test_state = test.attrib['status']
+        execution_time = str((int(test.attrib['stop']) - int(test.attrib['start'])) / 1000.0)
         try:
             desc = test.find('description').text
         except AttributeError:
             logger.warning("Lack of description for test: %s ", name, )
             desc = ''
         for node in test.iter():
-            if node.tag == 'label' and node.attrib['name'] == 'issue':
-                test_data = {}
+            test_data = {}
+            tag = node.tag
+            if tag == 'failure':
+                error = node.find('message').text
+            if tag == 'label' and node.attrib['name'] == 'issue':
                 i = node.attrib['value']
                 test_data['issues'] = i
                 test_data['name'] = name
                 test_data['description'] = desc
+                test_data['test status'] = test_state
+                test_data['time'] = execution_time + 's'
+                test_data['error'] = error
                 ISSUES.append(test_data)
 
 
@@ -44,7 +52,8 @@ def print_results(results, title):
     print()
     print(20 * '_' + title + 20 * '_')
     for issue in results:
-        print(issue['issues'], issue['name'], issue['description'], sep=', ')
+        print(issue['issues'], issue['name'], issue['description'], issue['test status'], issue['time'], issue['error'],
+              sep=', ')
     print()
 
 
@@ -56,7 +65,8 @@ def export_results_to_csv(results, file_name, path=os.path.dirname(__file__)):
         csv_output = open(destination_file, 'a', newline='')
         wr = csv.writer(csv_output, delimiter=';')
         for issue in results:
-            wr.writerow([issue['issues'], issue['name'], issue['description']])
+            wr.writerow([issue['issues'], issue['name'], issue['description'], issue['test status'], issue['time'],
+                         issue['error']])
         logger.info("Output csv created: %s ", destination_file)
     except PermissionError as e:
         logger.error("\n%s \nCannot save results to %s. \nReason: %s. \nClose file and try again. \n%s " % (
